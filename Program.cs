d33fd254f6d40e5bc0fae95e5d9f8992f35d0071
Trippie-Redd <anma07002@utb.vaxjo.se>
2024-08-28 11:16:49 +0200

    Intro

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿//Två tal
+int GetNumberFromConsole(string prompt) {
+    Console.Write(prompt);
+    string numberText = Console.ReadLine();
+
+    try {
+        return int.Parse(numberText);
+    } catch {
+        Console.WriteLine("Tweaking");
+        return GetNumberFromConsole(prompt);
+    }
+}
+
+ int FirstNum = GetNumberFromConsole("Tal 1? ");
+ int LastNum = GetNumberFromConsole("Tal 2? ");
+
+string Number = "Summan: " + (FirstNum + LastNum).ToString();
+string Number2 = "Differensen: " + (FirstNum - LastNum).ToString();
+Console.WriteLine(Number);
+Console.WriteLine(Number2);
+
+// Fullt Namn
+Console.Write("Förnamn? ");
+ string FirstName = Console.ReadLine();
+
+Console.Write("Efternamn? ");
+ string LastName = Console.ReadLine();
+
+string Name = FirstName + " " + LastName;
+Console.Write(Name);
